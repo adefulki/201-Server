a7092c94e043dbe08e87003a5210905fad2defc0
Risa Aprilia <risaapriliaa@gmail.com>
2017-05-31 13:17:52 +0700

    update c_pembeli

--- application/controllers/C_pembeli.php
+++ application/controllers/C_pembeli.php
@@ -37,8 +37,10 @@ class c_pembeli extends CI_Controller
      */
     function display_akun_pembeli()
     {
-        $obj=json_decode(file_get_contents('php://input');
-        $id_pembeli=$obj->{'id_pembeli'};
+        $obj=json_decode(file_get_contents('php://input'),true);
+        //$obj=json_decode($json);
+        $id_pembeli=$obj['id_pembeli']; // meminta array
+        //$id_pembeli=$obj->{'id_pembeli'};
 
         $pembeli = $this->Pembeli_model->get_pembeli($id_pembeli);
 
@@ -60,13 +62,13 @@ class c_pembeli extends CI_Controller
         /*
          * mendecode json kedalam variabel obj
          */
-        $obj= json_decode(file_get_contents('php://input');
+        $obj= json_decode(file_get_contents('php://input'),true);
 
         /*
          * memisahkan atribut
          */
-        $id_pembeli=$obj->{'id_pembeli'};
-        $nama_pembeli=$obj->{'nama_pembeli'};
+        $id_pembeli=$obj['id_pembeli'];
+        $nama_pembeli=$obj['nama_pembeli'];
 
         $arr = array(
             'NAMA_PEMBELI'=>$nama_pembeli
@@ -76,9 +78,9 @@ class c_pembeli extends CI_Controller
     //edit no 2. Email pembeli
     function edit_email_pembeli()
     {
-        $obj= json_decode(file_get_contents('php://input');
-        $id_pembeli=$obj->{'id_pembeli'};
-        $email_pembeli=$obj->{'email_pembeli'};
+        $obj= json_decode(file_get_contents('php://input'),true);
+        $id_pembeli=$obj['id_pembeli'];
+        $email_pembeli=$obj['email_pembeli'];
 
         $arr = array(
             'EMAIL_PEMBELI'=>$email_pembeli
@@ -88,9 +90,9 @@ class c_pembeli extends CI_Controller
     //edit no 3. No HP pembeli
     function edit_nohp_pembeli()
     {
-        $obj= json_decode(file_get_contents('php://input');
-        $id_pembeli=$obj->{'id_pembeli'};
-        $nohp_pembeli=$obj->{'nohp_pembeli'};
+        $obj= json_decode(file_get_contents('php://input'),true);
+        $id_pembeli=$obj['id_pembeli'];
+        $nohp_pembeli=$obj['nohp_pembeli'];
 
         $arr = array(
             'NOHP_PEMBELI'=>$nohp_pembeli
@@ -100,9 +102,9 @@ class c_pembeli extends CI_Controller
     // edit no 4. Password pembeli
     function edit_password_pembeli()
     {
-        $obj= json_decode(file_get_contents('php://input');
-        $id_pembeli=$obj->{'id_pembeli'};
-        $password_pembeli=$obj->{'password_pembeli'};
+        $obj= json_decode(file_get_contents('php://input'),true);
+        $id_pembeli=$obj['id_pembeli'];
+        $password_pembeli=$obj['password_pembeli'];
 
         $arr = array(
             'PASSWORD_PEMBELI'=>$password_pembeli
@@ -112,9 +114,9 @@ class c_pembeli extends CI_Controller
     // edit no 5. Alamat Pembeli
     function edit_alamat_pembeli()
     {
-        $obj= json_decode(file_get_contents('php://input');
-        $id_pembeli=$obj->{'id_pembeli'};
-        $alamat_pembeli=$obj->{'alamat_pembeli'};
+        $obj= json_decode(file_get_contents('php://input'),true);
+        $id_pembeli=$obj['id_pembeli'];
+        $alamat_pembeli=$obj['alamat_pembeli'];
 
         $arr = array(
             'ALAMAT_PEMBELI'=>$alamat_pembeli
@@ -138,8 +140,8 @@ class c_pembeli extends CI_Controller
      */
     function display_obrolan()
     {
-        $obj=json_decode(file_get_contents('php://input');
-        $id_pembeli=$obj->{'id_pembeli'};
+        $obj=json_decode(file_get_contents('php://input'),true);
+        $id_pembeli=$obj['id_pembeli'];
         $i=0;
         foreach($this-> Obrolan_model->get_obrolan_by_id_pembeli($id_pembeli) as $item){
 
@@ -177,10 +179,10 @@ class c_pembeli extends CI_Controller
      * 3. Tipe Dagangan ( ini perlu engga ? )
      * 4. Status berjualan ( untuk menandakan aktif atau tidak)
      */
-    function display_berlangganan(file_get_contents('php://input')
+    function display_berlangganan()
     {
-        $obj=json_decode(file_get_contents('php://input');
-        $id_pembeli=$obj->{'id_pembeli'};
+        $obj=json_decode(file_get_contents('php://input'),true);
+        $id_pembeli=$obj['id_pembeli'];
         $i=0;
         foreach($this-> Pelanggan_model->get_pelanggan_by_pembeli($id_pembeli) as $item){
 
@@ -209,13 +211,13 @@ class c_pembeli extends CI_Controller
      */
     function set_pemberitahuan()
     {
-        $obj= json_decode(file_get_contents('php://input');
-        $id_pembeli=$obj->{'id_pembeli'};
-        $id_dagangan=$obj->{'id_dagangan'};
+        $obj= json_decode(file_get_contents('php://input'),true);
+        $id_pembeli=$obj['id_pembeli'];
+        $id_dagangan=$obj['id_dagangan'];
         $id_notifikasi=$id_pembeli . "_" . $id_dagangan; // id notifikasi gabungan dari id pembeli dan id pedagang, sehingga tidak ada duplikat id_notifikasi
-        $status_notifikasi=$obj->{'status_notifikasi'};
-        $ulangi_otomatis=$obj->{'ulangi_otomatis'};
-        $jarak=$obj->{'jarak'};
+        $status_notifikasi=$obj['status_notifikasi'];
+        $ulangi_otomatis=$obj['ulangi_otomatis'];
+        $jarak=$obj['jarak'];
 
         $arr= array(
             'ID_NOTIFIKASI'=>$id_notifikasi,
@@ -230,11 +232,11 @@ class c_pembeli extends CI_Controller
 
     function update_set_pemberitahuan()
     {
-        $obj = json_decode(file_get_contents('php://input');
-        $id_pembeli =$obj->{'id_pembeli'};
-        $id_dagangan = $obj->{'id_dagangan'};
+        $obj = json_decode(file_get_contents('php://input'),true);
+        $id_pembeli =$obj['id_pembeli'];
+        $id_dagangan = $obj['id_dagangan'];
         $id_notifikasi=$id_pembeli . "_" . $id_dagangan;
-        $status_notifikasi=$obj->{'status_notifikasi'};
+        $status_notifikasi=$obj['status_notifikasi'];
 
         $arr=array(
             "STATUS_NOTIFIKASI" => $status_notifikasi,
@@ -252,22 +254,22 @@ class c_pembeli extends CI_Controller
 
     function display_penilaian_produk()
     {
-        $obj = json_decode(file_get_contents('php://input');
-        $id_pembeli = $obj->{'id_pembeli'};
-        $id_produk = $obj->{'id_produk'};
+        $obj = json_decode(file_get_contents('php://input'),true);
+        $id_pembeli = $obj['id_pembeli'];
+        $id_produk = $obj['id_produk'];
         $id_penilaian = $id_pembeli . "_" . $id_produk;
         $i=0;
         foreach($this-> Penilaian_model->get_penilaian_by_id_penilaian($id_penilaian) as $item){
 
-            foreach($this->Produk_model->get_produk_by_id_produk($item['ID_PRODUK']) as $item2 ){
+            $produk = $this->Produk_model->get_produk_by_id_produk($item['ID_PRODUK']);
                 $arr[$i]= array(
-                    'nama_produk' => $item2['NAMA_PRODUK'],
-                    'foto_produk' => $item2['FOTO_PRODUK'],
+                    'nama_produk' => $produk['NAMA_PRODUK'],
+                    'foto_produk' => $produk['FOTO_PRODUK'],
                     'nilai_produk' => $item['NILAI_PRODUK'],
                 );
 
                 $i++;
-            }
+
             header('Content-Type: application/json');
             echo json_encode($arr);
         }
@@ -276,12 +278,12 @@ class c_pembeli extends CI_Controller
 
     function add_penilaian_produk()
     {
-        $obj = json_decode(file_get_contents('php://input');
-        $id_pembeli = $obj->{'id_pembeli'};
-        $id_produk = $obj->{'id_produk'};
+        $obj = json_decode(file_get_contents('php://input'),true);
+        $id_pembeli = $obj['id_pembeli'];
+        $id_produk = $obj['id_produk'];
         $id_penilaian = $id_pembeli . "_" . $id_produk;
-        $nilai_produk = $obj->{'nilai_produk'};
-        $deskripsi_penilaian_produk = $obj->{'deskripsi_penilaian_produk'};
+        $nilai_produk = $obj['nilai_produk'];
+        $deskripsi_penilaian_produk = $obj['deskripsi_penilaian_produk'];
 
         $arr = array(
             "ID_PEMBELI" => $id_pembeli,
@@ -301,5 +303,5 @@ class c_pembeli extends CI_Controller
     //yang kurang berdasarkan mockup
     // isi dari chatting obrolan, ngeklik obrolan(mockup 13-18)
     // pindai qr code (mockup 5)
-    // menampilkan halaman untuk menilai
+
 }
